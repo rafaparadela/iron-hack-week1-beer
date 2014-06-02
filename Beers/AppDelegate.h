8648e86558ca8b690b9f3da788d73481409911e5
Rafa Paradela <rafaparadela@MacBook-Pro-de-Rafa.local>
2014-06-02 17:30:18 +0200

    Initial Commit

---
+++ Beers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Beers
+//
+//  Created by Rafa Paradela on 02/06/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
